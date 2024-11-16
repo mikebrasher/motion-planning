0802caba1db3cbfc3924d4eebfbfa56e8a2827b3
mikebrasher <mike_brasher@hotmail.com>
2024-11-16 13:04:19 -0600

    renamed ArenaTree to Graph and implemented RRT algorithm which owns a Graph

--- rrt/src/tree.rs
+++ rrt/src/tree.rs
@@ -1,58 +1,88 @@
-use glam::Vec2;
+use rand::{rngs::StdRng, Rng, SeedableRng};
 use std::{error::Error, fmt};
 
-#[derive(Default)]
-pub struct Node {
-    index: usize,
-    position: Vec2,
-    parent: Option<usize>,
-    children: Vec<usize>,
+#[derive(Clone, Default, Debug)]
+pub struct Configuration {
+    pub x: f64,
+    pub y: f64,
 }
 
-pub struct ArenaTree {
-    arena: Vec<Node>,
-}
-
-impl ArenaTree {
-    pub fn new(position: Vec2) -> Self {
-        ArenaTree {
-            arena: vec![Node {
-                index: 0,
-                position,
-                ..Default::default()
-            }],
-        }
+impl Configuration {
+    pub fn new(x: f64, y: f64) -> Self {
+        Configuration { x, y }
     }
 
-    pub fn insert(&mut self, position: Vec2, parent: usize) -> Result<usize, InvalidParentError> {
+    pub fn distance(&self, _other: Configuration) -> f64 {
+        0.0
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct Vertex {
+    index: usize,
+    config: Configuration,
+    neighbors: Vec<usize>,
+}
+
+#[derive(Debug, Default)]
+pub struct Edge {
+    pub tail: Configuration,
+    pub head: Configuration,
+}
+
+#[derive(Debug, Default)]
+pub struct Graph {
+    arena: Vec<Vertex>,
+}
+
+impl Graph {
+    pub fn add_vertex(&mut self, config: Configuration) -> usize {
         let index = self.arena.len();
 
-        // use get to check bounds on parent index
-        match self.arena.get_mut(parent) {
-            Some(parent_node) => {
-                parent_node.children.push(index);
+        self.arena.push(Vertex {
+            index,
+            config,
+            ..Default::default()
+        });
 
-                self.arena.push(Node {
-                    index,
-                    position,
-                    parent: Some(parent),
-                    ..Default::default()
-                });
+        index
+    }
 
-                Ok(index)
-            }
-            None => return Err(InvalidParentError { parent }),
+    pub fn add_edge(&mut self, tail: usize, head: usize) -> Result<Edge, InvalidIndexError> {
+        // check bounds before pushing any neighbors
+        if tail >= self.arena.len() {
+            return Err(InvalidIndexError { index: tail });
+        }
+
+        if head >= self.arena.len() {
+            return Err(InvalidIndexError { index: head });
+        }
+
+        // arena contains both vertices, add bidirectional edge
+        self.arena[tail].neighbors.push(head);
+        self.arena[head].neighbors.push(tail);
+
+        Ok(Edge {
+            tail: self.arena[tail].config.clone(),
+            head: self.arena[head].config.clone(),
+        })
+    }
+
+    pub fn get_vertex(&self, index: usize) -> Result<&Vertex, InvalidIndexError> {
+        match self.arena.get(index) {
+            Some(vertex) => Ok(vertex),
+            None => Err(InvalidIndexError { index }),
         }
     }
 
-    pub fn closest_node(&self, target: Vec2) -> Option<usize> {
+    pub fn nearest_vertex(&self, target: Configuration) -> Option<usize> {
         self.arena
             .iter()
-            .map(|node| {
-                let dx = target.x - node.position.x;
-                let dy = target.y - node.position.y;
+            .map(|v| {
+                let dx = target.x - v.config.x;
+                let dy = target.y - v.config.y;
                 let dist2 = dx * dx + dy * dy;
-                (node.index, dist2)
+                (v.index, dist2)
             })
             .min_by(|(_, a), (_, b)| a.total_cmp(b))
             .map(|(index, _)| index)
@@ -60,15 +90,70 @@ impl ArenaTree {
 }
 
 #[derive(Debug)]
-pub struct InvalidParentError {
-    parent: usize,
+pub struct InvalidIndexError {
+    index: usize,
 }
 
-impl fmt::Display for InvalidParentError {
+impl fmt::Display for InvalidIndexError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let &InvalidParentError { parent } = self;
-        write!(f, "invalid parent index for node {parent}")
+        let &InvalidIndexError { index } = self;
+        write!(f, "invalid vertex index: {index}")
     }
 }
 
-impl Error for InvalidParentError {}
+impl Error for InvalidIndexError {}
+
+pub struct RapidlyExploringRandomTree {
+    rng: StdRng,
+    graph: Graph,
+    delta_max: f64,
+}
+
+impl RapidlyExploringRandomTree {
+    pub fn new(seed: u64) -> Self {
+        let rng = StdRng::seed_from_u64(seed);
+
+        let graph = Graph {
+            arena: vec![Vertex::default()],
+        };
+
+        RapidlyExploringRandomTree {
+            rng,
+            graph,
+            delta_max: 3.0,
+        }
+    }
+
+    pub fn random_configuration(&mut self) -> Configuration {
+        let x = 2.0 * self.rng.gen::<f64>() - 1.0;
+        let y = 2.0 * self.rng.gen::<f64>() - 1.0;
+
+        Configuration::new(x, y)
+    }
+
+    pub fn new_configuration(
+        &self,
+        near: usize,
+        target: Configuration,
+    ) -> Result<Configuration, InvalidIndexError> {
+        let near = self.graph.get_vertex(near)?;
+
+        // apply non-holonomic constraints
+        let config = if near.config.distance(target.clone()) < self.delta_max {
+            target
+        } else {
+            target
+        };
+
+        Ok(config)
+    }
+
+    pub fn increment(&mut self) -> Result<Edge, InvalidIndexError> {
+        let sample = self.random_configuration();
+        let parent = self.graph.nearest_vertex(sample.clone()).unwrap();
+        let config = self.new_configuration(parent, sample)?;
+        let child = self.graph.add_vertex(config.clone());
+
+        self.graph.add_edge(parent, child)
+    }
+}
